95b804e4b72ac2033e1a5c978c489fd47b285866
AthenaG536 <64632419+AthenaG536@users.noreply.github.com>
2020-05-21 15:51:45 +1200

    Initial commit

---
+++ app/src/main/java/com/example/birdseyeview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.birdseyeview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnGo;
+    TextView txtVwTitle, txtVwInstMessage;
+    EditText editTxtUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo = findViewById(R.id.btnGo);
+        txtVwTitle = findViewById(R.id.txtVwTitle);
+        txtVwInstMessage = findViewById(R.id.txtVwInstructionMessage);
+        editTxtUserName = findViewById(R.id.editTxtUserName);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+
+            }
+        });
+    }
+}
